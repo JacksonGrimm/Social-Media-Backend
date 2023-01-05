ba6c3dc15210497e1e318759bb3c83b4696eaba5
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-04 17:29:33 -0700

    seeds now seeds user with email and username

---
+++ utils/data.js
@@ -0,0 +1,27 @@
+const userNames = [
+  "FunnyKitten23",
+  "GoblinKing20",
+  "PixelatedUnicorn",
+  "SassySparrow87",
+  "DancingGorilla99",
+  "SparklingGemstone",
+  "CuriousOctopus32",
+  "LivelyLizard13",
+  "GleamingGalaxy59",
+  "FierceTiger42",
+];
+
+const emails = [
+  "funnykitten23@gmail.com",
+  "goblinking420@yahoo.com",
+  "pixelatedunicorn@hotmail.com",
+  "sassysparrow87@outlook.com",
+  "dancinggorilla99@aol.com",
+  "sparklinggemstone@icloud.com",
+  "curiousoctopus32@gmail.com",
+  "livelylizard13@yahoo.com",
+  "gleaminggalaxy69@hotmail.com",
+  "fiercetiger42@outlook.com",
+];
+
+module.exports = { userNames, emails };
--- utils/seeds.js
+++ utils/seeds.js
@@ -1,7 +1,31 @@
 const connection = require("../config/connection");
 const { User, Thought } = require("../models");
+const thought = require("../models/thought");
+const { userNames, emails } = require("./data");
+const userArr = [];
+
+//creates array of users objects
+const newUser = (userArr) => {
+  for (let i = 0; i < userNames.length; i++) {
+    username = userNames[i];
+    email = emails[i];
+    userArr.push({
+      username: username,
+      email: email,
+    });
+  }
+  return userArr;
+};
 
 connection.once("open", async () => {
-  await User.deleteMany({});
-  await Thought.deleteMany({});
+  //attempts to delete collections if it exists.
+  try {
+    await User.collection.drop();
+    await thought.collection.drop();
+  } catch (error) {
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+  }
+  //creates users;
+  await User.insertMany(newUser(userArr));
 });
